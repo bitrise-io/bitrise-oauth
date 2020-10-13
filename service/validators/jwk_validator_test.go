9331edffa1a4bfa0ebc4fab8e2dff1e3a18b9651
Istvan Kovacs <istvan.kovacs@bitrise.io>
2020-10-13 09:50:31 +0200

    Validator cache test implemented

--- service/validators/jwk_validator_test.go
+++ service/validators/jwk_validator_test.go
@@ -1,12 +1,26 @@
 package validators_test
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"testing"
+	"time"
 
+	auth0 "github.com/auth0-community/go-auth0"
 	"github.com/bitrise-io/bitrise-oauth/service/validators"
 	"github.com/gorilla/mux"
 	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	Authorization = "Authorization"
+	JWT_1         = "BEARER eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldNVlkwcWVyZmJGUC0zcllLdW55NUFQaXJmcnk0OG5QZWVYcnlQNzk5RmsifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.TG_KUN36-raydoUZH0IeF_-upVbRCLemD7Bt3BCWCrr51cJjsSKAkIKmnyFH4Ow_5pLzlaU4NRoQMeoAIF0VvH28P6hnQKvMmpEM-kQ0UMHnZvMfzuh7SvsvEAIaDhtEEOFfYNV5w0jWoQsAbrMw9vkKrPFqXatxBF1t_yvbW6x2SKEql_UmmN88oxfa_-DS2OrKWsyk2hakR6rnM-m8zTrqdsRndvAP25DeNiySHR_fyB53Dn7un-TO7KJENi5X_obGuXKjQY0C5JFkibR1RY4o9Rp04rdFQrv_PPBC2Ki0pIqDpKVaRbceNkC1BiMzz2zjNR2B6EWbdzPB24bgAA"
+	JWT_2         = "BEARER eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldNVlkwcWVyZmJGUC0zcllLdW55NUFQaXJmcnk0OG5QZWVYcnlQNzk5RmsxIn0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.XLWuYJ3xb71XGh3XXH0xpX18_Q8RWQDIjUs6EKYD1mX2KXkJIWKj_1m4gNziEdTm03tXFKuDCXhdnFB7L7NJeOtT9dVNtIfqkBR0cYd2KU6HitPck9qd5wz_JcaaPQttHfrVBxJVIaK7ifZFCjjlGSukkYQ7aQalEv2ZjTycXP7FVs7bDq39f1OWdw2rM6XurrjWm65uEwC9m2z08DdgPnmyzCFh0NE5WyMHkezcIl2DDHxJjmb0AZkdIYW1q-AbYs0CIlAemOnxW_or7uzgtATZ-GWE_WEJp_bOeTkZK3BLnShXhlRdKNaHJXCuBzfBwdUY24-x6mEPRKNBYPGW3w"
+	RequestUrl    = "https://bitrise.io/protected_route"
 )
 
 func ExampleJWK_Middleware() {
@@ -92,4 +106,59 @@ func ExampleJWK_MiddlewareFunc_echo() {
 	e.GET("/test", handler)
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func Test_GivenRequestWithTokenThatHasNotExpired_WhenRequestWithThisTokenIsValidatedMultipleTimes_ThenExpectCertsEndpointToBeCalledOnce(t *testing.T) {
+	// Given
+	counter := NewCounter(1)
+
+	testAuthServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, _ := httputil.DumpRequest(r, true)
+		fmt.Println(string(b))
+
+		switch r.URL.Path {
+		case "/certs":
+			counter.Increment()
+			addContentTypeAndTokenToResponse(w)
+		default:
+			fmt.Println("Request handled by the default endpoint")
+			w.WriteHeader(http.StatusOK)
+		}
+	}))
+	defer testAuthServer.Close()
+
+	validator := validators.NewJWK(validators.WithCustomJWKSURL(testAuthServer.URL+"/certs"), validators.WithCustomKeyCacher(auth0.NewMemoryKeyCacher(60*time.Second, 5)))
+
+	request := createRequestWithToken(JWT_1)
+
+	// When
+	validator.ValidateRequest(request)
+	validator.ValidateRequest(request)
+
+	// Then
+	assert.Equal(t, counter.Expected, counter.Actual)
+}
+
+func addContentTypeAndTokenToResponse(w http.ResponseWriter) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Write([]byte("{\"keys\": [{\"kid\": \"WMVY0qerfbFP-3rYKuny5APirfry48nPeeXryP799Fk\",\"kty\": \"RSA\",\"alg\": \"RS256\",\"use\": \"sig\",\"n\": \"kGb4ABWuOgQH7yCydKsqLjZ7-FrWOQ5tezQQofHs5jJYQPXnMalUgvY6v9c0GEBvzebbmkigcGw9e8NesOLnVaP4mkE6TYLDyuL1vDRP9bIQuVOQxDwqhDPmaFKFawxe0YLoFN_N6NOZBZJ69z2Mbhxsd9By4tr_-bR-seg-korL5NUf6KpLYdBeCDy1xK_DSia49vV-SYAG5cuxgejRc32fmmZnVFx7rs8nIIAUoaGAHhWGM7ZFRaxC96dFsVRRR85-TDeukkPi0_-Q2RtNwpoz-hP2g-p6Vl620z3KJYW6pO6ssT3Q8SY0LOoK6gPca7NW7qGvOGyWhM6yk4aqPQ\",\"e\": \"AQAB\",\"x5c\": [\"MIICmzCCAYMCBgF01MwbDDANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjAwOTI4MTI1ODAwWhcNMzAwOTI4MTI1OTQwWjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCQZvgAFa46BAfvILJ0qyouNnv4WtY5Dm17NBCh8ezmMlhA9ecxqVSC9jq/1zQYQG/N5tuaSKBwbD17w16w4udVo/iaQTpNgsPK4vW8NE/1shC5U5DEPCqEM+ZoUoVrDF7RgugU383o05kFknr3PYxuHGx30HLi2v/5tH6x6D6Sisvk1R/oqkth0F4IPLXEr8NKJrj29X5JgAbly7GB6NFzfZ+aZmdUXHuuzycggBShoYAeFYYztkVFrEL3p0WxVFFHzn5MN66SQ+LT/5DZG03CmjP6E/aD6npWXrbTPcolhbqk7qyxPdDxJjQs6grqA9xrs1buoa84bJaEzrKThqo9AgMBAAEwDQYJKoZIhvcNAQELBQADggEBACSleX2IEZqb4h05T9+W3BT2e8cRiX06T8pHk4o70GAooROvMeHu0+l+HlT6lYggerzxsxqYGA7KFOG6JmgFG5XLPlNJoHlX0NOCGfCbrh50Q1HV5TZsqubIUOHglos9/SotipiSSVncd02Uot27sqU3A1HR9qO3IxmTe+W5XIvmzn3Kofpyj5r9qzbSJMfW2YKCJ8n+lG0g184SQ1JtQ2zFwBdziHtO8eBTscadjnZy4WHTs7F9hh2xNGUVBsfDQ5JZooJaZEeGe9r9Fv46R5Py/SMZvcpp6PvNptN2ifXoPzcP6jAZVphRlv7DZXIjb+9UgN3fHcDZNTK3NvJWoDA=\"],\"x5t\": \"fp4lFC6SkVBMvUGHq2MgrvB20L0\",\"x5t#S256\": \"BN6wsc3PZ_XfJXKPqH-SGdhe7QmEz-fECiheWPvkEBA\"}]}"))
+}
+
+func createRequestWithToken(jwt string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, RequestUrl, nil)
+	request.Header.Add(Authorization, jwt)
+	return request
+}
+
+type InvokeCounter struct {
+	Expected int
+	Actual   int
+}
+
+func NewCounter(expected int) InvokeCounter {
+	return InvokeCounter{Expected: expected}
+}
+
+func (counter *InvokeCounter) Increment() {
+	counter.Actual++
 }
