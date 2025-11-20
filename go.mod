043577d23ef874cc0e2f617aa137ffaec8149aaa
Imre Kelényi <imre.kelenyi@gmail.com>
2025-11-20 14:37:14 +0100

    validator repo returns issuer; dayadog validator metrics

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 )
 
 require (
+	github.com/DataDog/datadog-go/v5 v5.8.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -20,6 +21,8 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
---
+++ metrics/datadog.go
@@ -0,0 +1,54 @@
+package metrics
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-go/v5/statsd"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type DatadogMetrics struct {
+	rawClient statsd.ClientInterface
+	logger    *zap.SugaredLogger
+}
+
+// nolint: govet
+type DatadogConfig struct {
+	StatsdHost     string `env:"STATSD_HOST,default=datadog"`
+	StatsdPort     string `env:"STATSD_PORT,default=8125"`
+	MetricsEnabled bool   `env:"METRICS_ENABLED,default=true"`
+}
+
+func NewDatadogMetrics(cfg DatadogConfig, logger *zap.SugaredLogger) (*DatadogMetrics, func(), error) {
+	if !cfg.MetricsEnabled {
+		return &DatadogMetrics{
+			rawClient: &statsd.NoOpClient{},
+			logger:    logger,
+		}, func() {}, nil
+	}
+
+	c, err := statsd.New(fmt.Sprintf("%s:%s", cfg.StatsdHost, cfg.StatsdPort))
+	if err != nil {
+		return &DatadogMetrics{}, nil, errors.Wrap(err, "create statsd client failed")
+	}
+
+	return &DatadogMetrics{
+		rawClient: c,
+		logger:    logger,
+	}, func() { _ = c.Close() }, nil
+}
+
+func (dm *DatadogMetrics) IncrRaw(name string, tags []string, rate float64) {
+	if err := dm.rawClient.Incr(name, tags, rate); err != nil {
+		dm.logger.Errorw("failed to increment raw metric", zap.String("metric", name), zap.Error(err))
+	}
+}
+
+func (dm *DatadogMetrics) IncrAuthValidationSucceededMetric(issuer string) {
+	dm.IncrRaw("bitrise.jwt_auth.validation_succeeded", []string{"iss:" + issuer}, 1)
+}
+
+func (dm *DatadogMetrics) IncrAuthValidationFailedMetric(issuer string) {
+	dm.IncrRaw("bitrise.jwt_auth.validation_failed", []string{"iss:" + issuer}, 1)
+}
