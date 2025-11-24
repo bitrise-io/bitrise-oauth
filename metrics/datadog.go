e8e7f6551ebd1d864833930b578e3c2f18f8da85
Imre Kelényi <imre.kelenyi@gmail.com>
2025-11-24 15:44:35 +0100

    error handling + separate Close()

--- metrics/datadog.go
+++ metrics/datadog.go
@@ -26,26 +26,32 @@ type Logger interface {
 	Errorw(msg string, keysAndValues ...interface{})
 }
 
-func NewDatadogMetrics(cfg DatadogConfig, logger Logger) (*DatadogMetrics, func(), error) {
+func NewDatadogMetrics(cfg DatadogConfig, logger Logger) (*DatadogMetrics, error) {
 	if !cfg.MetricsEnabled {
 		return &DatadogMetrics{
 			rawClient: &statsd.NoOpClient{},
 			logger:    logger,
-		}, func() {}, nil
+		}, nil
 	}
 
 	c, err := statsd.New(fmt.Sprintf("%s:%s", cfg.StatsdHost, cfg.StatsdPort))
 	if err != nil {
-		return &DatadogMetrics{}, nil, errors.Wrap(err, "create statsd client failed")
+		return &DatadogMetrics{}, errors.Wrap(err, "create statsd client failed")
 	}
 
 	return &DatadogMetrics{
 		rawClient: c,
 		logger:    logger,
-	}, func() { _ = c.Close() }, nil
+	}, nil
 }
 
 func (dm *DatadogMetrics) IncrRaw(name string, tags []string, rate float64) {
+	if name == "" {
+		dm.logger.Errorw("metric name is empty")
+
+		return
+	}
+
 	if err := dm.rawClient.Incr(name, tags, rate); err != nil {
 		dm.logger.Errorw("failed to increment raw metric", zap.String("metric", name), zap.Error(err))
 	}
@@ -63,6 +69,10 @@ func (dm *DatadogMetrics) IncrAuthValidationFailedMetric(issuer string) {
 	if issuer == "" {
 		issuer = unknownIssuerId
 	}
-	
+
 	dm.IncrRaw("bitrise.jwt_auth.validation_failed", []string{"iss:" + issuer}, 1)
+}
+
+func (dm *DatadogMetrics) Close() {
+	_ = dm.rawClient.Close()
 }
